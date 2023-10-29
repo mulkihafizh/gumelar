c187e8f28b6d7efc4941f9f21042f7cd92e3fbad
mulkihafizh <91298960+mulkihafizh@users.noreply.github.com>
2023-10-29 11:03:41 +0700

    Update testimonial.ts

--- utils/testimonial.ts
+++ utils/testimonial.ts
@@ -7,14 +7,14 @@ export type testimonialRule = {
 
 export const testimonials: testimonialRule[] = [
   {
-    name: "Tes1",
-    affiliation: "Teacher",
+    name: "Testi 1",
+    affiliation: "Student",
     comment:
       "Pie susunya sangat enak sekali saya sampai ketagihan pesan berkali-kali, harganya juga sesuai banget sama kantong pelajar, top banget deh pokoknya",
     stars: 5,
   },
   {
-    name: "Tes2",
+    name: "Testi 2",
     affiliation: "Student",
     comment:
       "Enak banget pie susunya, saya pesan 10 buah langsung habis dalam sehari, harganya juga murah banget, top banget deh pokoknya",
